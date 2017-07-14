8bcf097981637c3802ff40e5733e6d70635cc143
Ellen Cooper <ellencooper74@gmail.com>
2017-07-14 11:17:41 -0600

    implements shared form for company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,8 @@
 class Company < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :jobs, dependent: :destroy
+
+  def column_names
+    [:name]
+  end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,8 @@
 class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, presence: true
   belongs_to :company
+
+  def column_names
+    [:title, :description, :level_of_interest, :city]
+  end
 end
