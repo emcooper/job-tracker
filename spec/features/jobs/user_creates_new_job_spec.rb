f341006773fac2254a184fc16ef5b61f6e0a409b
Ellen Cooper <ellencooper74@gmail.com>
2017-07-15 18:12:37 -0600

    adds new category button to new job page

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -21,4 +21,17 @@ describe "User creates a new job" do
     expect(page).to have_content("Denver")
     expect(page).to have_content("Technology")
   end
+
+  scenario "and can create a new category" do
+    company = Company.create!(name: "ESPN")
+    category = create(:category, title: "Technology")
+
+    visit new_company_job_path(company)
+    click_on "New Category"
+    fill_in "category_title", with: "Human Resources"
+    click_on "Create Category"
+    visit new_company_job_path(company)
+
+    expect(page).to have_content("Human Resources")
+  end
 end
