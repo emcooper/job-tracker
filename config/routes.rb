be60b95554cb574492ef3ef0f2791378c75013cd
Ellen Cooper <ellencooper74@gmail.com>
2017-07-17 00:06:18 -0600

    added root route to company index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "/jobs", to: "jobs#sort"
 
+  root 'companies#index'
+
   resources :companies do
     resource :contacts, only: [:create]
     resources :jobs do
